b48f9bf64b717c32268194b6050bb37ae39efd8d
Pierre Joseph <pierre_joseph@brown.edu>
2026-04-18 09:43:13 -0400

    navbar fix

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -20,8 +20,8 @@ export function Navbar() {
   const navLinks = [
     { href: "/", label: "HOME" },
     { href: "/team", label: "OUR TEAM" },
-    { href: "/issues", label: "BSJ ISSUES" },
-    { href: "/sections", label: "SECTIONS" },
+    { href: "/issues", label: "ISSUES" },
+    { href: "/sections", label: "ARTICLES" },
     { href: "/events", label: "EVENTS" },
     { href: "/archives", label: "ARCHIVES" }
   ];
